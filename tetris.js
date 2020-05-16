8bb94a6be0e0a65753231e40d2a5b0271b60ac29
Arianna Basha <ariannagbasha@gmail.com>
2020-05-16 11:22:47 -0500

    Figuring out the shapes for the arrays

--- tetris.js
+++ tetris.js
@@ -5,7 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 10;
     const ScoreDisplay = document.querySelector('#score') 
     const StartBin = document.querySelector('#start-button') 
-    // const lTertominos = "firstShape";
+    const lTertominos = [
+        [1, width + 1, width*2 + 1, 2 ],
+        [width, width + 1, width + 2, width * 2 +2],
+        [1, width + 1, width*2 + 1, width * 2],
+        [width, width * 2, width*2 + 1, width * 2*2]
+    ]
     // const zTertominos = "secondShape";
     // const oTertominos = "thirdShape";
     // const iTertominos = "fourShape";
