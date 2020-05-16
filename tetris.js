33a8ac027299ca7676899e4675bfe9ec36265e6c
Arianna Basha <ariannagbasha@gmail.com>
2020-05-16 11:00:25 -0500

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const width = 10;
+    const ScoreDisplay = document.querySelector('#score') 
+    const StartBin = document.querySelector('#start-button') 
+    // const lTertominos = "firstShape";
+    // const zTertominos = "secondShape";
+    // const oTertominos = "thirdShape";
+    // const iTertominos = "fourShape";
+    // const tTertominos = "fiveShape";
+    
+    // const tetrominos = [lTertominos, zTertominos, oTertominos, iTertominos, tTertominos]
 
 
+    
 })
